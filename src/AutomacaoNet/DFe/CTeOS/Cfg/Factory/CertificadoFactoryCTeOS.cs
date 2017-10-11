74ed9ad5aaec93b7a483a1711f40d8f5df1e7aea
robertorp <robertoalvespereira18@gmail.com>
2017-10-11 10:09:44 -0300

    FactoryCertificadoCTeOS ajustes

--- src/AutomacaoNet/DFe/CTeOS/Cfg/Factory/CertificadoFactoryCTeOS.cs
+++ src/AutomacaoNet/DFe/CTeOS/Cfg/Factory/CertificadoFactoryCTeOS.cs
@@ -1,5 +1,8 @@
-﻿using AutomacaoNet.Cfg;
+﻿using System;
+using AutomacaoNet.Cfg;
+using AutomacaoNet.Cfg.Flags;
 using DFe.CertificadosDigitais.Implementacao;
+using TpCert = DFe.Utils.Flags.TipoCertificado;
 
 namespace AutomacaoNet.DFe.CTeOS.Cfg.Factory
 {
@@ -11,7 +14,26 @@ namespace AutomacaoNet.DFe.CTeOS.Cfg.Factory
 
             configCertDigital.LocalArquivo = configuracao.DadosCertificadoDigital.Arquivo;
             configCertDigital.Senha = configuracao.DadosCertificadoDigital.Senha;
-            
+            configCertDigital.ArrayBytesArquivo = configuracao.DadosCertificadoDigital.CertBytes;
+            configCertDigital.Serial = configuracao.DadosCertificadoDigital.Serial;
+
+            switch (configuracao.DadosCertificadoDigital.TipoCertificado)
+            {
+                case TipoCertificado.A1Repositorio:
+                    configCertDigital.TipoCertificado = TpCert.A1Repositorio;
+                    break;
+                case TipoCertificado.A1Arquivo:
+                    configCertDigital.TipoCertificado = TpCert.A1Arquivo;
+                    break;
+                case TipoCertificado.A1ByteArray:
+                    configCertDigital.TipoCertificado = TpCert.A1ByteArray;
+                    break;
+                case TipoCertificado.A3:
+                    configCertDigital.TipoCertificado = TpCert.A3;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
 
 
             return configCertDigital;
