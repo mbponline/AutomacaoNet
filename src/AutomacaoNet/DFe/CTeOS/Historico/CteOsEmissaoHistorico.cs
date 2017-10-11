be38a90f548076cb9505a4c1c78a2600394d4d68
robertorp <robertoalvespereira18@gmail.com>
2017-10-11 14:42:01 -0300

    Feito classe para acesso ao banco de dados com Nhibernate Feito mapeamento da classe de acesso ao banco de dados

---
+++ src/AutomacaoNet/DFe/CTeOS/Historico/CteOsEmissaoHistorico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AutomacaoNet.DFe.CTeOS.Historico
+{
+    public class CteOsEmissaoHistorico
+    {
+        public int Id { get; set; }
+        public string CnpjEmitente { get; set; }
+        public string XmlEnvio { get; set; }
+        public string XmlRetorno { get; set; }
+        public string XmlAutorizado { get; set; }
+        public int NumeroFiscal { get; set; }
+        public string Chave { get; set; }
+        public bool IsFinalizou { get; set; }
+        public bool IsAutorizado { get; set; }
+        public DateTime EnviadoEm { get; set; }
+        public DateTime AutorizadoEm { get; set; }
+        public short CodigoStatusSefaz { get; set; }
+        public string MotivoSefaz { get; set; }
+        public string Referencia { get; set; }
+    }
+}
